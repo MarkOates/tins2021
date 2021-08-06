d022ad62207bcdecdd7d5f51d49254fded332f17
Mark Oates <markoates0@gmail.com>
2021-08-06 11:27:14 -0400

    * Have ApplicationController initialize an allegro setup

--- src/ApplicationController.cpp
+++ src/ApplicationController.cpp
@@ -1,7 +1,8 @@
 
 
 #include <ApplicationController.hpp>
-
+#include <allegro5/allegro.h>
+#include <allegro5/allegro_color.h>
 
 
 
@@ -18,6 +19,11 @@ ApplicationController::~ApplicationController()
 
 std::string ApplicationController::run_program()
 {
+   al_init();
+   ALLEGRO_DISPLAY *display = al_create_display(1920, 1080);
+   al_clear_to_color(al_color_name("green"));
+   al_flip_display();
+   sleep(1);
    return "Hello World!";
 }
 
